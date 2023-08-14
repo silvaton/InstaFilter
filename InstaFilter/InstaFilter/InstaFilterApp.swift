2996457fffdf157540f49eff0a04bd01589af866
silvaton <tdasilva@ext.laligatech.com>
2023-08-14 12:24:07 +0200

    001 - Created the project

---
+++ InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Ton Silva on 14/8/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
